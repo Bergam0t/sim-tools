d66f1bd35b7d013a1940ef4614efd83a417915b9
amyheather <a.heather2@exeter.ac.uk>
2025-02-27 15:55:39 +0000

    tests(reps): add tests for output_analysis functions

---
+++ tests/dummy_model.py
@@ -0,0 +1,38 @@
+"""Dummy simulation model used for testing
+"""
+
+import numpy as np
+
+
+class DummySimulationModel():
+    def __init__(self, mean=100, std_dev=5):
+        """
+        Initialises the dummy model.
+
+        Parameters:
+        ----------
+        mean: float, optional (default=100)
+            Mean for the normal distribution.
+        std_dev: float, optional (default=5)
+            Standard deviation for the normal distribution.
+        """
+        self.mean = mean
+        self.std_dev = std_dev
+        self.rand = np.random.default_rng()
+
+    def single_run(self, replication_number) -> float:
+        """
+        Simulate a single replication with controlled randomness.
+
+        Parameters:
+        ----------
+        replication_number: int
+            Dummy replication number, used as random seed when sampling from
+            the normal distribution.
+
+        Returns:
+        --------
+            float
+        """
+        return np.random.default_rng(replication_number).normal(
+            loc=self.mean, scale=self.std_dev)
---
+++ tests/test_replications_back.py
@@ -0,0 +1,84 @@
+"""Replication back tests
+
+Back testing for code related to selecting the number of replications.
+
+Credit: Some of these tests are adapted from-
+    Heather, A. Monks, T. (2025). Python DES RAP Template. GitHub.
+    https://github.com/pythonhealthdatascience/rap_template_python_des.
+"""
+
+import pytest
+
+from tests.dummy_model import DummySimulationModel
+from sim_tools.output_analysis import (ReplicationsAlgorithm,
+                                       confidence_interval_method)
+
+
+@pytest.mark.parametrize("mean, std_dev, expected_n_reps", [
+    (7, 0.2, 5),
+    (10, 2, 60),
+    (50, 1, 4)
+])
+def test_ci_method(mean, std_dev, expected_n_reps):
+    """
+    Use DummySimulationModel to check that results from
+    confidence_interval_method are consistent with those previously generated.
+
+    Parameters:
+    -----------
+    mean: float
+        Mean for the normal distribution in the dummy model.
+    std_dev: float
+        Standard deviation for the normal distribution in the dummy model.
+    expected_n_reps: int
+        Expected result from confidence_interval_method.
+    """
+    model = DummySimulationModel(mean=mean, std_dev=std_dev)
+
+    replications = [model.single_run(rep) for rep in range(1, 101)]
+
+    n_reps, _ = confidence_interval_method(
+        replications=replications,
+        alpha=0.05,
+        desired_precision=0.05,
+        min_rep=3,
+        decimal_places=2)
+
+    assert n_reps == expected_n_reps, (
+        f"Expected {expected_n_reps} replications but got {n_reps}")
+
+
+@pytest.mark.parametrize("mean, std_dev, expected_n_reps", [
+    (100, 4, 5),
+    (1, 1, 332),
+    (44, 27, 171)
+])
+def test_algorithm(mean, std_dev, expected_n_reps):
+    """
+    Use DummySimulationModel to check that results from ReplicationsAlgorithm
+    are consistent with those previously generated.
+
+    Parameters:
+    -----------
+    mean: float
+        Mean for the normal distribution in the dummy model.
+    std_dev: float
+        Standard deviation for the normal distribution in the dummy model.
+    expected_n_reps: int
+        Expected result from ReplicationsAlgorithm.
+    """
+    model = DummySimulationModel(mean=mean, std_dev=std_dev)
+
+    analyser = ReplicationsAlgorithm(
+        alpha=0.05,
+        half_width_precision=0.1,
+        initial_replications=5,
+        look_ahead=10,
+        replication_budget=500,
+        verbose=False
+    )
+
+    n_reps = analyser.select(model)
+
+    assert n_reps == expected_n_reps, (
+        f"Expected {expected_n_reps} replications but got {n_reps}")
